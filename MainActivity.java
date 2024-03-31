39ae1035bbe2c6a2542af2ea67330d90455a913b
Harshit <89185268+Harry1008HS@users.noreply.github.com>
2024-03-31 14:13:09 +0530

    fgh

--- MainActivity.java
+++ MainActivity.java
@@ -4,12 +4,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -23,10 +25,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
- EditText e1,e2;
- Button submit;
- TextView textView;
- private static final String url="";
+ EditText e1,e2,e3,e4;
+ Button submit,nextpeg;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,47 +35,48 @@ public class MainActivity extends AppCompatActivity {
 
         e1=findViewById(R.id.e1);
         e2=findViewById(R.id.e2);
-        submit=findViewById(R.id.sumit);
-        textView=findViewById(R.id.tv);
+        e3=findViewById(R.id.e3);
+        e4=findViewById(R.id.e4);
+        nextpeg=findViewById(R.id.nextpage);
+        nextpeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FetchData.class));
+            }
+        });
 
+        submit=findViewById(R.id.sumit);
+        RequestQueue requestQueue=Volley.newRequestQueue(this);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getData(e1.getText().toString(),
-                        e2.getText().toString());
+         StringRequest stringRequest=new StringRequest(Request.Method.POST, "https://durable-aids.000webhostapp.com/insert.php", new Response.Listener<String>() {
+             @Override
+             public void onResponse(String response) {
+                 Toast.makeText(MainActivity.this, "get response"+response, Toast.LENGTH_SHORT).show();
+             }
+         }, new Response.ErrorListener() {
+             @Override
+             public void onErrorResponse(VolleyError error) {
+
+                 Toast.makeText(MainActivity.this, "Error"+error, Toast.LENGTH_SHORT).show();
+             }
+         }){
+             @Nullable
+             @Override
+             protected Map<String, String> getParams() throws AuthFailureError {
+                  super.getParams();
+                  Map<String, String> map=new HashMap<>();
+                 map.put("user_name",e1.getText().toString());
+                 map.put("user_phone",e2.getText().toString());
+                 map.put("user_email",e3.getText().toString());
+                 map.put("user_password",e4.getText().toString());
+                 return map;
+
+             }
+         };
+         requestQueue.add(stringRequest);
             }
         });
     }
-
-   public void getData(final  String name, final String email) {
-       StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-           @Override
-           public void onResponse(String response) {
-               e1.setText("");
-               e2.setText("");
-                textView.setText(response);
-           }
-       }, new Response.ErrorListener() {
-           @Override
-           public void onErrorResponse(VolleyError error) {
-               e1.setText("");
-               e2.setText("");
-               textView.setText(error.toString());
-               textView.setTextColor(Color.RED);
-           }
-       }
-       ){
-           @Override
-           protected Map<String, String> getParams() throws AuthFailureError {
-               Map<String ,String > map=new HashMap<String,String>();
-               map.put("name",name);
-               map.put("email",email);
-               return map;
-           }
-       };
-       RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
-       queue.add(stringRequest);
-    }
-
-
 }
